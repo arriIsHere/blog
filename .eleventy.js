abe71205568e5cfdc2fc2ad2acc068d00ceb1e77
bblais23 <ben.blais6@gmail.com>
2019-02-09 11:18:55 -0500

    Move configuration to eleventy config file.

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,21 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter('date', function(value, format) {
     return Moment(value).format(format);
   });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    },
+    templateFormats: [
+      "md",
+      "html",
+      "css",
+      "jpg",
+      "png",
+      "gif",
+      "njk"
+    ],
+    passthroughFileCopy: true
+  }
 };
