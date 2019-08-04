2d983317f1f9bcf8ea8732b531d9352c24d33dc4
Ben Blais <ben.blais6@gmail.com>
2019-08-04 18:47:00 -0400

    Add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 const Moment = require('moment');
+const SyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
+
+  // Install plugin for syntax highlighting
+  eleventyConfig.addPlugin(SyntaxHighlight);
+
   eleventyConfig.addFilter('date', function(value, format) {
     return Moment(value).format(format);
   });
