ee6f754b2467fe46dc8c0a5d4200a35b35d1e56f
Benjamin Blais <ben.blais6@gmail.com>
2020-03-21 20:32:43 -0400

    Convert spaces to tabs

--- .eleventy.js
+++ .eleventy.js
@@ -3,27 +3,27 @@ const SyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
 
-  // Install plugin for syntax highlighting
-  eleventyConfig.addPlugin(SyntaxHighlight);
+	// Install plugin for syntax highlighting
+	eleventyConfig.addPlugin(SyntaxHighlight);
 
-  eleventyConfig.addFilter('date', function(value, format) {
-    return Moment(value).format(format);
-  });
+	eleventyConfig.addFilter('date', function(value, format) {
+		return Moment(value).format(format);
+	});
 
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist'
-    },
-    templateFormats: [
-      "md",
-      "html",
-      "css",
-      "jpg",
-      "png",
-      "gif",
-      "njk"
-    ],
-    passthroughFileCopy: true
-  }
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist'
+		},
+		templateFormats: [
+			"md",
+			"html",
+			"css",
+			"jpg",
+			"png",
+			"gif",
+			"njk"
+		],
+		passthroughFileCopy: true
+	}
 };
